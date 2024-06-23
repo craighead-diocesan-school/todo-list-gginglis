5904ba8dfb8752dbbc81033ff8a48258381d6434
gginglis <inglisge23@craighead.school.nz>
2024-06-24 10:29:47 +1200

    adds tasks to array

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,36 @@
 <script>
-  import Header from '$lib/Header.svelte'
+  import Header from "$lib/Header.svelte";
+  let tasks = [];
+
+  function addTask() {
+    tasks = [...tasks, ""];
+  }
 </script>
 
 <Header />
 
 <main>
-  <h2>SvelteKit</h2>
+  <button on:click={addTask}>📝 Add</button>
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+  {#each tasks as task, index}
+    <div class="task">
+      <input bind:value={tasks[index]} />
+    </div>
+  {/each}
+  <!-- <h2>SvelteKit</h2>
+
+  <p>
+    Welcome to coding with SvelteKit, a modern JavaScript framework that makes
+    it easy to code great apps.
+  </p> -->
 </main>
 
 <footer>
   <p>&copy; Craighead Diocesan School 2024</p>
 </footer>
+
+<style>
+  .task {
+    display: block;
+  }
+</style>
